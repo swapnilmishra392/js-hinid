84055d4f5d778aa04ad5591a84a4f4677d451f44
Swapnil Mishra <74293429+swapnilmishra392@users.noreply.github.com>
2025-06-23 18:55:57 +0000

    test

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "Skmishra@gmail.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+let accountState 
+// accountId = 3 not allowed
+
+/*
+prefer not to use var
+because of issues in block of scope & functional scope
+
+*/
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
