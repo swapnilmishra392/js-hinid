48ffb88313390681371c21c8d4eeb3069d1f0e31
Swapnil Mishra <skmishra241126@gmail.com>
2025-06-25 23:39:59 +0530

    first push

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strick"; //treat all JS code as newer version
+
+// alert(3 +3) //we are using nodejs not browser 
+
+console.log("swapnil")
+
+let name = "Swapnil"
+let age = 18
+let islogged = false
+
+// number 2 to the power of 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined =>
+// syllbom 
+
+// object
+
+console.log(typeof(undefined)); //undefined
+console.log(typeof(null)); // oject 
