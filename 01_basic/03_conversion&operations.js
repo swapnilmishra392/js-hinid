37ab9abe3e495f35949b2f29516cd1bdc1d8ba85
Swapnil Mishra <skmishra241126@gmail.com>
2025-06-26 00:31:12 +0530

    r

--- 01_basic/03_conversion&operations.js
+++ 01_basic/03_conversion&operations.js
@@ -7,3 +7,17 @@ console.log(typeof valueInNuber)
 
 console.log(valueInNuber)
 
+// ************************ OPERATIONS***********************
+
+let value = 3 
+let negvalue = -value
+console.log(negvalue)
+
+let str1 = "hello"
+let str2 = "Swapnil"
+let str3 = str1 + str2 
+console.log(str3)
+
+console.log(1+"2")
+console.log("1"+2 +2)
+console.log(1+2 +"2")
